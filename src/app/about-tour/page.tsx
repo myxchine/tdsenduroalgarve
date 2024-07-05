c6f3c0169f47f5c4681a5dd2baedbba5f7b75407
Michael Dos Santos <michael.dozzantoz@gmail.com>
2024-07-05 14:37:48 +0100

    t

--- src/app/about-tour/page.tsx
+++ src/app/about-tour/page.tsx
@@ -22,28 +22,28 @@ export default function AboutTour() {
         buttonText2={null}
         buttonLink2={null}
       />
-      <div className="flex flex-col gap-4 py-12 w-full max-w-6xl mx-auto">
+      <div className="flex flex-col gap-4 py-12 w-full max-w-6xl mx-auto md:gap-8">
         <section className="flex flex-col p-6 gap-4">
-          <h4 className="text-3xl    uppercase font-tds tracking-wider w-full">
+          <h4 className="text-3xl    uppercase font-tds tracking-wider w-full md:text-5xl">
             Tour adventure in the Algarve region.
           </h4>
-          <p className="w-full text-sm text-black/80 ">
+          <p className="w-full text-sm text-black/80 md:text-lg">
             Join one of our experienced guides for this exciting full-day ride
             through the beautiful Algarve region while riding a KTM EXC-300! We
             will provide all the necessary safety gear and share our best riding
             techniques. Lunch is included in the ride​.
           </p>
           <Link href={`/bikes/ktm-exc-300`} className="w-full">
-            <button className="flex flex-col w-full max-w-[200px] p-2 px-4 rounded bg-tdsRed text-white font-tds hover:bg-tdsRed/80 text-center justify-center items-center ">
+            <button className="flex flex-col w-full max-w-[200px] p-2 px-4 rounded bg-tdsRed text-white font-tds hover:bg-tdsRed/80 text-center justify-center items-center md:p-4 md:px-8 md:text-lg">
               Available Packages
             </button>
           </Link>
         </section>
         <section className="flex flex-col p-6 gap-4">
-          <h4 className="text-3xl    uppercase font-tds tracking-wider w-full">
+          <h4 className="text-3xl    uppercase font-tds tracking-wider w-full md:text-5xl">
             Included
           </h4>
-          <div className="w-full text-sm text-black/80 ">
+          <div className="w-full text-sm text-black/80 md:text-lg">
             <ul>
               <li>KTM EXC 300 TPI</li>
               <li>Full Day Tour Guide</li>
