8745bee3e28a8addfa2d13816675c9d34b50f9be
Michael Dos Santos <michael.dozzantoz@gmail.com>
2024-07-05 14:25:11 +0100

    t

--- src/app/about-tour/page.tsx
+++ src/app/about-tour/page.tsx
@@ -22,47 +22,54 @@ export default function AboutTour() {
         buttonText2={null}
         buttonLink2={null}
       />
-      <section className="flex flex-col p-6 gap-4">
-        <h4 className="text-3xl    uppercase font-tds tracking-wider w-full">
-          Tour adventure in the Algarve region.
-        </h4>
-        <p className="w-full text-sm text-black/80 ">
-          Join one of our experienced guides for this exciting full-day ride
-          through the beautiful Algarve region while riding a KTM EXC-300! We
-          will provide all the necessary safety gear and share our best riding
-          techniques. Lunch is included in the ride​.
-        </p>
-      </section>
-      <section className="flex flex-col p-6 gap-4">
-        <h4 className="text-3xl    uppercase font-tds tracking-wider w-full">
-          Included
-        </h4>
-        <div className="w-full text-sm text-black/80 ">
-          <ul>
-            <li>KTM EXC 300 TPI</li>
-            <li>Full Day Tour Guide</li>
-            <li>All protective and riding equipment</li>
-            <li>Lunch</li>
-            <li>Water and snacks</li>
-            <li>Fuel</li>
-          </ul>
-        </div>
-      </section>
-      <section className="flex flex-col p-6 pt-0 gap-4">
-        <Image
-          src="/images/bikes/enduro.png"
-          alt="enduro tour"
-          width={400}
-          height={400}
-          priority={true}
-          className="w-full object-cover rounded "
-        />
-        <Link href={`/bikes/ktm-exc-300`} className="w-full">
-          <button className="flex flex-col w-full p-2 px-4 rounded bg-tdsRed text-white font-tds hover:bg-tdsRed/80 text-center justify-center items-center ">
-            View Bike
-          </button>
-        </Link>
-      </section>
+      <div className="flex flex-col gap-4 py-12 w-full max-w-6xl mx-auto">
+        <section className="flex flex-col p-6 gap-4">
+          <h4 className="text-3xl    uppercase font-tds tracking-wider w-full">
+            Tour adventure in the Algarve region.
+          </h4>
+          <p className="w-full text-sm text-black/80 ">
+            Join one of our experienced guides for this exciting full-day ride
+            through the beautiful Algarve region while riding a KTM EXC-300! We
+            will provide all the necessary safety gear and share our best riding
+            techniques. Lunch is included in the ride​.
+          </p>
+          <Link href={`/bikes/ktm-exc-300`} className="w-full">
+            <button className="flex flex-col w-full max-w-[200px] p-2 px-4 rounded bg-tdsRed text-white font-tds hover:bg-tdsRed/80 text-center justify-center items-center ">
+              Available Packages
+            </button>
+          </Link>
+        </section>
+        <section className="flex flex-col p-6 gap-4">
+          <h4 className="text-3xl    uppercase font-tds tracking-wider w-full">
+            Included
+          </h4>
+          <div className="w-full text-sm text-black/80 ">
+            <ul>
+              <li>KTM EXC 300 TPI</li>
+              <li>Full Day Tour Guide</li>
+              <li>All protective and riding equipment</li>
+              <li>Lunch</li>
+              <li>Water and snacks</li>
+              <li>Fuel</li>
+            </ul>
+          </div>
+        </section>
+        <section className="flex flex-col p-6 pt-0 gap-4">
+          <Image
+            src="/images/bikes/enduro.png"
+            alt="enduro tour"
+            width={400}
+            height={400}
+            priority={true}
+            className="w-full object-cover rounded "
+          />
+          <Link href={`/bikes/ktm-exc-300`} className="w-full">
+            <button className="flex flex-col w-full p-2 px-4 rounded bg-tdsRed text-white font-tds hover:bg-tdsRed/80 text-center justify-center items-center ">
+              View Bike
+            </button>
+          </Link>
+        </section>
+      </div>
     </section>
   );
 }
