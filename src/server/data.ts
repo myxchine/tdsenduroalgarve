6e14362d7f111172ef6e4ac2b51d6c26993d4a6c
Michael Dos Santos <michael.dozzantoz@gmail.com>
2024-07-07 05:48:59 +0100

    t

--- src/server/data.ts
+++ src/server/data.ts
@@ -175,56 +175,6 @@ export const bikes = [
       { id: 3, price: 270, days: 1, riders: 3 },
     ],
   },
-  {
-    id: 2,
-    category: "Adventure",
-    name: "ktm adv 790 2019",
-    slug: "ktm-adv-790",
-    image: "/images/bikes/adventure.jpg",
-    description:
-      "Being the most well-rounded machine you'll ever come across, feel free to cruise on the highway or explore the beautiful dirt roads of Algarve.",
-    features: [
-      "High-performance",
-      "Lightweight",
-      "Versatile",
-      "Durable",
-      "Reliable",
-      "Comfortable",
-    ],
-    packageName: "rental + tour",
-    packageInfo: "No bike license required off-road.",
-    packageMaxRiders: 5,
-    packages: [
-      { id: 1, price: 300, days: 1, riders: 1 },
-      { id: 2, price: 280, days: 1, riders: 2 },
-      { id: 3, price: 270, days: 1, riders: 3 },
-    ],
-  },
-  {
-    id: 3,
-    category: "Street",
-    name: "ktm duke 125 2022",
-    slug: "ktm-duke-125",
-    image: "/images/bikes/duke.jpeg",
-    description:
-      "The KTM Duke is known for being the apex predator in its domain, boasting the most powerful 125 engine on the market.",
-    features: [
-      "High-performance",
-      "Lightweight",
-      "Versatile",
-      "Durable",
-      "Reliable",
-      "Comfortable",
-    ],
-    packageName: "rental + tour",
-    packageInfo: "No bike license required off-road.",
-    packageMaxRiders: 5,
-    packages: [
-      { id: 1, price: 300, days: 1, riders: 1 },
-      { id: 2, price: 280, days: 1, riders: 2 },
-      { id: 3, price: 270, days: 1, riders: 3 },
-    ],
-  },
 ];
 
 export const tours = [
