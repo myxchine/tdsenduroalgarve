575b8bf0e63f169a8a2a8a3173c844804f241661
Michael Dos Santos <michael.dozzantoz@gmail.com>
2024-07-08 00:54:26 +0100

    t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ const TDS = localFont({
   variable: "--font-tds",
 });
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL!),
   title: {
     default: "TDS Enduro Bike Tours Algarve, Portugal",
     template: "%s - TDS Enduro Bike Tours Algarve, Portugal",
