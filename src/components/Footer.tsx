d3d8b66ab644161424a87e0309a1d082b4549a78
Michael Dos Santos <michael.dozzantoz@gmail.com>
2024-07-09 10:56:11 +0100

    t

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
-
+import { TDSIcon } from "./icons";
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center justify-center w-full py-8 border-t border-black/10 py-10 pb-6">
+    <footer className="flex flex-col items-center justify-center w-full py-8 border-t border-black/10 py-10 pb-6 md:py-20 md:pb-6">
       <div className="flex flex-col items-center justify-between gap-8 w-full max-w-6xl px-6 md:gap-16">
-        <h2 className="text-4xl w-full font-tds md:text-6xl md:text-center">
-          TDS <span className="text-tdsRed ">Enduro</span> Algarve
-        </h2>
+        <div className="flex flex-row gap-4 items-center md:gap-8">
+          <TDSIcon className="w-auto h-[100px] md:w-32 md:h-32" />
+          <h2 className="text-4xl w-full font-tds md:text-6xl md:text-center">
+            TDS <span className="text-tdsRed ">Enduro</span> Algarve
+          </h2>
+        </div>
 
         <section className="flex grid grid-cols-2 md:grid-cols-3 w-full gap-8 md:gap-12 md:flex-row ">
           <nav className="flex flex-col items-start gap-4 text-sm w-full">
@@ -54,7 +57,7 @@ const Footer = () => {
           experience. Join us for an adventure and create lasting memories on
           two wheels.
         </p>
-        <div className="flex flex-col items-center gap-4 text-xs md:text-sm">
+        <div className="flex flex-col items-center gap-4 text-xs md:text-sm mt-4">
           <p>&copy; 2024 TDS Enduro Algarve. All rights reserved.</p>
         </div>
       </div>
