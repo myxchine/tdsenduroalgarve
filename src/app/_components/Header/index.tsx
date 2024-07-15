654bf5e0e7e6bf89c5b67a1cadd613b9bb0f2831
Michael Dos Santos <michael.dozzantoz@gmail.com>
2024-07-15 20:39:23 +0100

    t

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -3,7 +3,7 @@ import Menu from "./Menu";
 import AnnouncementBanner from "./AnnouncementBanner";
 export default function Header() {
   return (
-    <header className="flex flex-col items-center w-full  bg-transparent z-1000000 ">
+    <header className="flex flex-col items-center w-full  bg-transparent z-0 ">
       <AnnouncementBanner />
       <section className="w-full md:hidden block">
         <MobileMenu />
--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="text-foreground flex flex-col items-center justify-center gap-0  md:gap-4 relative  tmt-[-88px]  tmd:mt-[-96px]">
+    <section className="text-foreground flex flex-col items-center justify-center gap-0  md:gap-4 relative  ">
       <HeroImage />
       <Title />
     </section>
@@ -20,7 +20,7 @@ function HeroImage() {
         priority={true}
         width={800}
         height={800}
-        className=" object-cover object-left  w-full h-[400px] md:h-[600px] lg:h-[800px] "
+        className=" object-cover object-left  w-full h-[400px] md:h-[600px] lg:h-[800px] mt-[-88px]  md:mt-[-96px] z-[-1]"
       />
     </section>
   );
@@ -30,7 +30,7 @@ function Title() {
   return (
     <section className="w-full md:max-w-2xl lg:max-w-md right-0 lg:absolute xl:right-32 py-4 ">
       <h1
-        className={` text-foreground text-3xl text-balance text-center md:text-4xl  uppercase p-4 font-tds tracking-wider lg:text-left lg:text-5xl text-balance lg:text-background`}
+        className={` text-foreground text-2xl text-balance text-center md:text-4xl  uppercase p-4 font-tds tracking-wider lg:text-left lg:text-5xl text-balance lg:text-background`}
       >
         Off Road Bike tours & Rentals in Portugal
       </h1>
